4fca0ca767bf26718813168bfe64e6966b65119c
kubernautis <edward@neuralinternet.ai>
2024-09-28 21:48:20 -0400

    restarting process if it hasn't set weights for 500 blocks

--- healthchecker.py
+++ healthchecker.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import threading
 import subprocess
+from typing import Optional
 
 import psutil
 from log_reader import LogReader
@@ -11,6 +12,8 @@ from datetime import datetime
 import pytz
 import socket
 import traceback
+import bittensor
+from retry import retry
 
 
 def get_formatted_date_now() -> str:
@@ -21,10 +24,36 @@ def get_formatted_date_now() -> str:
 def log(something):
     print(f"{get_formatted_date_now()} {something}")
 
+
+def get_subtensor(network: Optional[str] = "wss://entrypoint-finney.opentensor.ai:443", config: Optional[bittensor.config] = None, log_verbose=False):
+    """create a subtensor and retry 3 times by increasing the sleep by 2 between each retries"""
+
+    @retry(tries=3, delay=1, backoff=2)
+    def _create_subtensor(_network: Optional[str] = None, _config: Optional[bittensor.config] = None, _log_verbose=False):
+        return bittensor.subtensor(network=_network, config=_config, log_verbose=_log_verbose)
+
+    subtensor = _create_subtensor(network, config, log_verbose)
+    return subtensor
+
+class NotRegistered(Exception):
+    pass
+
+def is_registered(metagraph: bittensor.metagraph, ss58_address: str):
+    if ss58_address not in metagraph.hotkeys:
+        print(f"{ss58_address} is not registered.")
+        raise NotRegistered()
+    else:
+        uid = metagraph.hotkeys.index(ss58_address)
+        print(f"{ss58_address} is registered with {uid} uid.")
+        return uid
+
+BLOCKS_WITHOUT_SETTING_WEIGHTS_THRESHOLD = 500
+
 class HealthChecker:
     def __init__(self, pm2_process_names, disk_limit: int):
         log("Initializing HealthChecker")
         self.hostname = socket.gethostname()
+        self.ss58_address = "5Hb63SvXBXqZ8zw6mwW1A39fHdqUrJvohXgepyhp2jgWedSB"
         self.disk_state = self.check_disk_space(disk_limit)
         log("Listing pm2 processes")
         pm2_processes = self.list_pm2_process()
@@ -67,6 +96,7 @@ class HealthChecker:
     def monitor_processes(self):
         while True:
             log("Monitoring processes")
+            subtensor = get_subtensor(log_verbose=True)
             for log_reader in self.threads:
                 try:
                     last_line_timestamp = log_reader.get_stats().get("last_line_timestamp")
@@ -78,7 +108,26 @@ class HealthChecker:
                         log(f"PM2 process {log_reader.pm2_process.name} is ok, last log line was {int(last_line_delta)} seconds ago")
                 except Exception as e:
                     print(e)
+                netuid = log_reader.pm2_process.netuid
+                if netuid and netuid > 0:
+                    try:
+                        metagraph = subtensor.metagraph(netuid)
+                        curr_block = subtensor.block
+
+                        try:
+                            validator_uid = is_registered(metagraph, self.ss58_address)
+                        except NotRegistered:
+                            print(f"netuid: {netuid:<3} | not registered on netuid")
+                            return
+                    
+                        updated_block_since = curr_block - metagraph.last_update[validator_uid]
+                        if updated_block_since >= BLOCKS_WITHOUT_SETTING_WEIGHTS_THRESHOLD:
+                            log(f"Restarting pm2 process {log_reader.pm2_process.name} cause it hasn't set weights in {BLOCKS_WITHOUT_SETTING_WEIGHTS_THRESHOLD} blocks")
+                            log_reader.restart_pm2_process()
+                    except Exception as e:
+                        print(e)
             time.sleep(150)
+
 
     def stop(self):
         self.is_running = False
